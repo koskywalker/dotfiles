6167acbecafaad1c25bad00c42a9e9da7e6441d8
koskywalker <k.muraka.jp@gmail.com>
2020-12-18 01:08:25 +0900

    一通りの設定ファイルを更新

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
-ln -sf ~/workspace/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/workspace/dotfiles/.vscode/settings.json ~/Library/Application\ Support/Code/User
+
+# zsh
+ln -sf ~/workspace/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/workspace/dotfiles/zsh/prezto/.p10k.zsh ~/.p10k.zsh
+ln -sf ~/workspace/dotfiles/zsh/prezto/.zpreztorc ~/.zpreztorc
+
+# vscode
+ln -sf ~/workspace/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User
--- zsh/prezto/.p10k.zsh
+++ zsh/prezto/.p10k.zsh
@@ -175,11 +175,11 @@
   # Separator between same-color segments on the right.
   typeset -g POWERLEVEL9K_RIGHT_SUBSEGMENT_SEPARATOR='\uE0B3'
   # Separator between different-color segments on the left.
-  typeset -g POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR='\uE0B0'
+  typeset -g POWERLEVEL9K_LEFT_SEGMENT_SEPARATOR='\uE0B1'
   # Separator between different-color segments on the right.
   typeset -g POWERLEVEL9K_RIGHT_SEGMENT_SEPARATOR='\uE0B2'
   # The right end of left prompt.
-  typeset -g POWERLEVEL9K_LEFT_PROMPT_LAST_SEGMENT_END_SYMBOL='\uE0B0'
+  typeset -g POWERLEVEL9K_LEFT_PROMPT_LAST_SEGMENT_END_SYMBOL='\uE0B1'
   # The left end of right prompt.
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_FIRST_SEGMENT_START_SYMBOL='\uE0B2'
   # The left end of left prompt.
