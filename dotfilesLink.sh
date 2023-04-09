3850946bde50b6857f6428031d8fa33893c5221e
koskywalker <k.muraka.jp@gmail.com>
2023-04-09 18:29:31 +0900

    更新

--- dotfilesLink.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# zsh
-ln -sf ~/spaceDev/koskywalker/dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/spaceDev/koskywalker/dotfiles/zsh/prezto/.zpreztorc ~/.zpreztorc
-ln -sf ~/spaceDev/koskywalker/dotfiles/zsh/powerlevel10k/.p10k.zsh ~/.p10k.zsh
-
-# vscode
-ln -sf ~/spaceDev/koskywalker/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User
